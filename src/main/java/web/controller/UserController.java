fc2505536aa9699a60d00bddff82fa589c4fe6c7
RibbleStripe <ribblestripeps3@gmail.com>
2022-07-27 12:54:48 +0300

    Initial commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,57 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import web.model.User;
+import web.service.UserService;
+
+@Controller
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+
+    @GetMapping("/index")
+    public String allUsers(Model model) {
+        model.addAttribute("user", userService.getAllUsers());
+        return "/index";
+    }
+
+    @GetMapping("/create")
+    public String createUserForm(@ModelAttribute User user) {
+        return "/create";
+    }
+
+    @PostMapping("/createUser")
+    public String createUser(@ModelAttribute User user) {
+        userService.createUser(user);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/update/{id}")
+    public String editUser(@PathVariable long id, Model model) {
+        model.addAttribute("user", userService.getUser(id));
+        return "/update";
+    }
+
+    @PostMapping("/userUpdate")
+    public String updateUser(@ModelAttribute User user) {
+        userService.updateUser(user);
+        return "redirect:/";
+    }
+
+    @GetMapping("/delete/{id}")
+    public String deleteUser(@PathVariable long id) {
+        userService.deleteUser(id);
+        return "redirect:/";
+    }
+}
---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,63 @@
+package web.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name = "firstName")
+    private String name;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "salary")
+    private int salary;
+
+    public User() {
+    }
+
+    public User(long id, String name, String lastName, int salary) {
+        this.id = id;
+        this.name = name;
+        this.lastName = lastName;
+        this.salary = salary;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
